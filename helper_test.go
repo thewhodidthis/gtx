04552d55214d5a028616d333bb656ec4ad8015f2
thewhodidthis <thewhodidthis@fastmail.com>
2022-11-24 18:55:50 +0000

    helper: start adding tests

---
+++ helper_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestContains(t *testing.T) {
+	words := strings.Fields("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
+
+	for _, w := range words {
+		match := contains(words, w)
+
+		if !match {
+			t.Errorf("failed to match %v", w)
+			t.Fail()
+		}
+	}
+}
